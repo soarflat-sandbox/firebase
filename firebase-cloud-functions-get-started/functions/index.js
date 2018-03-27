960543748874f3da496a46d2623c519e9c347764
soarflat <soarflat777@gmail.com>
2018-03-28 01:54:43 +0900

    wip

---
+++ firebase-cloud-functions-get-started/functions/index.js
@@ -0,0 +1,13 @@
+// Cloud Functionsを作成してトリガを設定するためのFirebase SDKのCloud Functions
+const functions = require('firebase-functions');
+
+// Firebase Realtime DatabaseにアクセスするためのFirebase Admin SDK
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
