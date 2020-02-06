460eecdaf47cfa1586d303d6b27c4709218838bf
soarflat <soarflat777@gmail.com>
2020-02-06 10:30:18 +0900

    chore: Google Cloud Functions を利用したチャットクライアントの雛形を追加

--- firebase-cloud-functions/README.md
+++ firebase-cloud-functions/README.md
@@ -2,10 +2,10 @@
 
 [Cloud Functions for Firebase](https://codelabs.developers.google.com/codelabs/firebase-cloud-functions/#0)
 
-Google Cloud Functionsを利用して[チャット](../firebase-web)を改善する。
+Google Cloud Functions を利用して[チャット](../firebase-web)を改善する。
 
-- Firebase SDKを使用してGoogle Cloud Functionsを作成する。
-- Auth、Cloud Storage、およびRealtime Databaseイベントに基づいてCloud Functionsをトリガーする。
-- WebアプリケーションにFirebase Cloud Messagingサポートを追加する。
+- Firebase SDK を使用して Google Cloud Functions を作成する。
+- Auth、Cloud Storage、および Realtime Database イベントに基づいて Cloud Functions をトリガーする。
+- Web アプリケーションに Firebase Cloud Messaging サポートを追加する。
 
 ### MEMO
--- firebase-cloud-functions/functions/index.js
+++ firebase-cloud-functions/functions/index.js
@@ -1,76 +1,23 @@
-// Cloud FunctionsとFirebase Admin modulesをimportする
+/**
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-// Google Cloud Functions用のFirebase SDKをimportする
-const functions = require('firebase-functions');
+// TODO(DEVELOPER): Import the Cloud Functions for Firebase and the Firebase Admin modules here.
 
-// Firebase Admin SDKをインポートして初期化する
-const admin = require('firebase-admin');
-// Firebase Admin SDKはCloud Functionsにデプロイすると自動的に設定できる
-admin.initializeApp(functions.config().firebase);
+// TODO(DEVELOPER): Write the addWelcomeMessages Function here.
 
-// チャットアプリでユーザーが初めてログインしたときに実行される関数を追加する
-// ユーザーを歓迎するチャットメッセージを追加する
-// Firebaseアプリケーションで初めてユーザがサインインするたびに実行されるfunctions.auth().onCreateを利用する
-exports.addWelcomeMessages = functions.auth.user().onCreate(event => {
-  const user = event.data;
-  console.log('A new user signed in for the first time.');
-  const fullName = user.displayName || 'Anonymous';
+// TODO(DEVELOPER): Write the blurOffensiveImages Function here.
 
-  // 新しいウェルカムメッセージをデータベースに保存する
-  // それがチャットクライアントに表示される
-  return admin.database().ref('messages').push({
-    name: 'Firebase Bot',
-    photoUrl: '/images/firebase-logo.png', // Firebase logo
-    text: `${fullName} signed in for the first time! Welcome!` // Using back-ticks.
-  }).then(() => console.log('Welcome message written to database.'));
-});
-
-exports.sendNotifications = functions.database.ref('/messages/{messageId}').onCreate(event => {
-  const snapshot = event.data;
-
-  // 通知の詳細
-  const text = snapshot.val().text;
-  const payload = {
-    notification: {
-      title: `${snapshot.val().name} posted ${text
-        ? 'a message'
-        : 'an image'}`,
-      body: text
-        ? (text.length <= 100
-          ? text
-          : text.substring(0, 97) + '...')
-        : '',
-      icon: snapshot.val().photoUrl || '/images/profile_placeholder.png',
-      click_action: `https://${functions.config().firebase.authDomain}`
-    }
-  };
-
-  // Firebaseリアルタイムデータベースからすべてのユーザのデバイストークンを収集し、それぞれに通知を送信する
-  return admin.database().ref('fcmTokens').once('value').then(allTokens => {
-    if (allTokens.val()) {
-      // 全てのトークンをリストにする
-      const tokens = Object.keys(allTokens.val());
-
-      // すべてのトークンに通知を送信する
-      return admin.messaging().sendToDevice(tokens, payload).then(response => {
-
-        // 各メッセージについて、エラーがあったかどうかをチェックする
-        // ユーザーから取得したトークンがブラウザやデバイスで使用されていない場合にエラーが発生する
-        // たとえば、ユーザーがブラウザセッションの通知許可を取り消した場合など
-        const tokensToRemove = [];
-        response.results.forEach((result, index) => {
-          const error = result.error;
-          if (error) {
-            console.error('Failure sending notification to', tokens[index], error);
-            // 登録されていないトークンをクリーンアップする
-            if (error.code === 'messaging/invalid-registration-token' ||
-              error.code === 'messaging/registration-token-not-registered') {
-              tokensToRemove.push(allTokens.ref.child(tokens[index]).remove());
-            }
-          }
-        });
-        return Promise.all(tokensToRemove);
-      });
-    }
-  });
-});
+// TODO(DEVELOPER): Write the sendNotifications Function here.
